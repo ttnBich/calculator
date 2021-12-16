07d88b7a05fb9f72ae8975e73b1c2602f83e783d
ngoc bich <bich03032001@gmail.com>
2021-12-16 11:30:57 +0700

    Update Form1

--- Calucater/Form1.cs
+++ Calucater/Form1.cs
@@ -16,5 +16,15 @@ namespace Calucater
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
